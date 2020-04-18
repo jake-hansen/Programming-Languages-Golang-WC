ef89d3c12df57eab1e264cd48d5d6eeacde37e66
Jake Hansen <hansenjake12@gmail.com>
2020-04-17 21:47:58 -0500

    Add file reading functionality
    For debugging purposes, file contents are printed out to the console.

--- src/app/utilities/filereader.go
+++ src/app/utilities/filereader.go
@@ -1,7 +1,18 @@
 package utilities
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 func Readfile(filename string) {
 	fmt.Println("filepath is", filename)
+
+	data, error := ioutil.ReadFile(filename)
+	if error != nil {
+		fmt.Println("Error reading file", error)
+		return
+	}
+	fmt.Println(string(data))
 }
+ 
